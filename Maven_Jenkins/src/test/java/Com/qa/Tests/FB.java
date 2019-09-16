5dea863b5bfb370670e1cb4530b28cd45c10d587
HP <HP@LAPTOP-E2FFA7B3>
2019-09-16 12:10:32 +0530

    @16th Sept

---
+++ Maven_Jenkins/src/test/java/Com/qa/Tests/FB.java
@@ -0,0 +1,28 @@
+package Com.qa.Tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.Test;
+
+
+public class FB {
+
+	@Test
+	public  void fb() throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		
+		driver.manage().window().maximize();
+		driver.get("https://fb.com");
+		Thread.sleep(7000);
+		
+		
+		driver.close();
+		}	
+	
+
+	
+	
+}
